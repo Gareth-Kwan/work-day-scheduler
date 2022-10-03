f6ec120ec92447fd3f6e89328413a481ff384470
GK <gareth0201@gmail.com>
2022-10-03 00:48:45 -0400

    Fixed bug with local storage

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -1,9 +1,19 @@
 //
-// variables Decorations
+// variables declaration
 //
 let timeBlockEL = document.querySelector(".timeBlock");
 let btnEL = document.querySelector(".btn");
-let task = [];
+const saveButtons = document.querySelectorAll(".saveBtn");
+const schedule = document.querySelector(".schedule");
+const timeBlocks = document.querySelectorAll(".timeBlock");
+
+// get the saved data
+const savedData = JSON.parse(window.localStorage.getItem("taskData")); // How do I get this to show my stuff
+
+// colors
+const pastColor = "#6F6868";
+const presentColor = "#EE0D0D";
+const futureColor = "#0DEE0F";
 
 //
 // Functions
@@ -15,34 +25,63 @@ function updateDate() {
   $("#currentDay").text(date);
 }
 
+// do something with the save data
+function renderSavedData() {
+  // if saved data exists (is not null) then run the function
+  if (savedData) {
+    savedData.forEach(function (item, index) {
+      if (item !== "") {
+        // get the timeblock which corresponds to the current saved data element.
+        const blockToUpdate = timeBlocks[index];
+
+        blockToUpdate.value = item;
+      }
+    });
+  }
+}
+
 //Render Background Colors according to the time
 function renderBackgroundColor() {
   let currentTime = parseInt(moment().format("H"));
   let id = document.querySelector("textarea");
-  let timeEL = parseInt(id.getAttribute("id")); // This is not working, how to get the element ID and parse it as a number
-  console.log(timeEL);
+  // let timeEL = parseInt(id.getAttribute("id")); // This is not working, how to get the element ID and parse it as a number
+  // console.log(timeEL);
 
-  For each statement{
-    if (timeEL < currentTime) {
-      timeBlockEL.style.backgroundColor = "#B2BABB"; //Green
-    } else if (timeEL === currentTime) {
-      timeBlockEL.style.backgroundColor = "#F1C40F "; //Yellow
+  // create array of time blocks to loop through
+  console.log("Current time: ", parseInt(moment().format("H"))); // log the current time for reference
+
+  timeBlocks.forEach(function (block) {
+    const blockTime = parseInt(block.id);
+
+    if (blockTime < currentTime) {
+      block.style.backgroundColor = pastColor; //Green
+    } else if (blockTime === currentTime) {
+      block.style.backgroundColor = presentColor; //Yellow
     } else {
-      timeBlockEL.style.backgroundColor = "#CD5C5C"; //Red
+      block.style.backgroundColor = futureColor; //Red
     }
-  }
+  });
 }
 
 //
 //Event Listener
 //
+schedule.addEventListener("click", function (event) {
+  // check if the click target is inside a button.
+  const saveBtn = event.target.closest(".saveBtn");
+  if (saveBtn) {
+    const newData = [];
 
-// Eventlistener for local storage when we click the save button
-btnEL.addEventListener("click", function () {
-  localStorage.setItem("task", JSON.stringify(task)); // This is also not working, not saving to the local storage
+    // get all the text from all the textAreas and push to an array
+    timeBlocks.forEach(function (block) {
+      newData.push(block.value);
+    });
+
+    window.localStorage.setItem("taskData", JSON.stringify(newData));
+  }
 });
 
 // Invoke Functions
 updateDate();
 renderBackgroundColor();
-JSON.parse(window.localStorage.getItem("task")); // How do I get this to show my stuff
+renderSavedData();
